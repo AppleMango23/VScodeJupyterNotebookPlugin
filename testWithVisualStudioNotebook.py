42ff4bd114ffc5593add70aa27af1c10a585873b
Noah Yek <ninjayek@gmail.com>
2021-04-01 11:44:56 +0800

    first commit

---
+++ testWithVisualStudioNotebook.py
@@ -0,0 +1,16 @@
+# %%
+import os
+%pylab inline
+print("First line");
+
+# %%
+plot([0,1,2], [0,1,4])
+
+
+# %%
+import pandas as pd
+
+
+# %%
+df = pd.DataFrame({"one":1, "two":2}, {"one":11,"two":22})
+# %%
